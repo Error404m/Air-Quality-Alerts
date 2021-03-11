72e5a0f0c28bc29c66a509e1ce349c867d3d3808
MrityunjayaTripathi <58814328+Error404m@users.noreply.github.com>
2021-03-11 09:12:41 +0530

    Create login.js

---
+++ login.js
@@ -0,0 +1,17 @@
+const signUpButton = document.getElementById("signUp");
+const signInButton = document.getElementById("signIn");
+const container = document.getElementById("container");
+
+signUpButton.addEventListener("click", () =>
+  container.classList.add("right-panel-active")
+);
+
+signInButton.addEventListener("click", () =>
+  container.classList.remove("right-panel-active")
+);
+
+const popup = document.querySelector("#popup");
+
+popup.addEventListener("click", function () {
+  alert(" This product is under development ");
+});
