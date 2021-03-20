1bab4666c702ea2dbbac63d71cec5e6d4ffbb381
Error404R <64827299+Error404R@users.noreply.github.com>
2021-03-20 22:50:11 +0530

    Update login.js

--- login.js
+++ login.js
@@ -10,7 +10,7 @@ signInButton.addEventListener("click", () =>
   container.classList.remove("right-panel-active")
 );
 
-const popup = document.querySelector("#");
+const popup = document.querySelector("#popup");
 
 popup.addEventListener("click", function (){
   alert(" This product is under development ");
